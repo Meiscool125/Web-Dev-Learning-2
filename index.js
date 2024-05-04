56ccb13bfdd0537306dee686c5f2c5d94df26100
Meiscool125 <dathan.kenworthy@gmail.com>
2024-05-04 15:21:20 -0500

    learned some more

--- index.js
+++ index.js
@@ -23,3 +23,52 @@ age = Number(age);
 
 //constants
 const constantVar = "immortal";
+
+//random nums
+let randomNumber = Math.random();
+let min = 50;
+let max = 100;
+let betweenRandom = Math.round(Math.random() * (max - min)) + min;
+
+// if statements
+let sigma = true
+
+if(sigma){
+    console.log("Sigma is true");
+}
+else if(sigma == 18){
+    console.log("yes, sigma = 18")
+}
+else{
+    console.log("Sigma is false");
+}
+
+//checked property
+document.getElementById("submitButton2").onclick = function(){
+    let checkbox1 = document.getElementById("checkbox1");
+    let option1 = document.getElementById("radiobutton1");
+    let option2 = document.getElementById("radiobutton2");
+    let option3 = document.getElementById("radiobutton3");
+    let sigmaStatusText = document.getElementById("sigmaStatusText")
+    let optionChosenText = document.getElementById("optionChosenText")
+
+    if(checkbox1.checked){
+        sigmaStatusText.textContent = "Sigma status: Sigma";
+    }
+    else{
+        sigmaStatusText.textContent = "Sigma status: Beta";
+    }
+
+    if(option1.checked){
+        optionChosenText.textContent = "Option chosen: Option 1";
+    }
+    else if(option2.checked){
+        optionChosenText.textContent = "Option chosen: Option 2";
+    }
+    else if(option3.checked){
+        optionChosenText.textContent = "Option chosen: Option 3";
+    }
+    else{
+        optionChosenText.textContent = "No option chosen";
+    }
+}
